3e492e85b06c76c5a06c9d258b6275a049f5d493
SpesCoin <35995649+SpesCoin@users.noreply.github.com>
2018-09-17 14:47:11 +0100

    Add files via upload

---
+++ q/hashrate/index.php
@@ -0,0 +1,13 @@
+<?php 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, 'http://18.220.99.199:55490/getinfo');
+$result = curl_exec($ch);
+$obj = json_decode($result, TRUE);
+curl_close($ch);
+//print_r($obj);
+$difficulty = $obj['difficulty'];
+$hashrate = round($difficulty / 100);
+print_r($hashrate);
+?>
