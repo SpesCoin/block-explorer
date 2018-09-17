1b9791d8ebbe961dfceaba66debc5991d92c252f
SpesCoin <35995649+SpesCoin@users.noreply.github.com>
2018-09-17 14:39:46 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+var api = 'http://18.220.99.199:55490';
+//var apiList = ["http://18.220.99.199:55490"];
+var blockTargetInterval = 100;
+var coinUnits = 10000000000;
+var symbol = 'SPES';
+var refreshDelay = 30000;
+// pools stats by MainCoins
+var networkStat = {
+    "spes": [
+		
+		["pool.myspes.org", "http://pool.myspes.org:8117/stats"]
+    ]
+};
